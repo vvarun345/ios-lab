ac4ccf2344de81064cd585c765a5f962b8fc541a
chetanbc <cchavannavar@gmail.com>
2019-09-11 13:02:33 +0530

    first commit

---
+++ fp.c
@@ -0,0 +1,11 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(void)
+{
+	printf(1, "WOW It really Works\n");
+	printf(1, "It is always fun to work with xv6 \n");
+	exit();
+}
